34b5c626671cd66657bbe1c670cc810873b0706b
niya1123 <yoheikurioka@gmail.com>
2020-07-08 08:36:05 +0900

    rulesパッケージなどの追加

--- app/sns_project/sns_project/settings.py
+++ app/sns_project/sns_project/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rules.apps.AutodiscoverRulesConfig',
 ]
 
 MIDDLEWARE = [
@@ -121,3 +122,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 STATIC_URL = '/staticfiles/'
 STATIC_ROOT = '/usr/src/app/staticfiles/'
+
+AUTHENTICATION_BACKENDS = (
+    'rules.permissions.ObjectPermissionBackend',
+    'django.contrib.auth.backends.ModelBackend',
+)
