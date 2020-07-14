23ad77e89e0b8b8dbedfcfcaa120bc31cbc3edb3
niya1123 <yoheikurioka@gmail.com>
2020-07-14 19:28:57 +0900

    タイトル変更だけした

--- app/sns_project/sns_project/urls.py
+++ app/sns_project/sns_project/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+admin.site.site_header = 'SNSサイト' 
+
 urlpatterns = [
     path('accounts/', include('accounts.urls')),
     path('', include('sns_app.urls')),
