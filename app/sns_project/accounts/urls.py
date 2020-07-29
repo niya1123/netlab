5bd8d836d0d7d5531b60c546f3fe0f39dc0dd3f2
niya1123 <yoheikurioka@gmail.com>
2020-07-29 17:38:32 +0900

    AS_VIEW()の追加

--- app/sns_project/accounts/urls.py
+++ app/sns_project/accounts/urls.py
@@ -7,10 +7,10 @@ app_name = 'accounts'
 urlpatterns = [
     path('test/', views.test, name='test'),
     path('signup/', views.signup, name='signup'),
-    path('login/', views.Login, name='login'),
-    path('logout/', views.Logout, name='logout'),
-    path('profile/', views.Profile, name='profile'),
-    path('user_create/', views.UserCreate, name='user_create'),
-    path('user_create/done', views.UserCreateDone, name='user_create_done'),
-    path('user_create/complete/<token>/', views.UserCreateComplete, name='user_create_complete'),
+    path('login/', views.Login.as_view(), name='login'),
+    path('logout/', views.Logout.as_view(), name='logout'),
+    path('profile/', views.Profile.as_view(), name='profile'),
+    path('user_create/', views.UserCreate.as_view(), name='user_create'),
+    path('user_create/done', views.UserCreateDone.as_view(), name='user_create_done'),
+    path('user_create/complete/<token>/', views.UserCreateComplete.as_view(), name='user_create_complete'),
 ]
