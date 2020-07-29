a78da6b5e54a4b801351ef1b6cead8a21d3ce630
niya1123 <yoheikurioka@gmail.com>
2020-07-29 17:28:58 +0900

    ユーザ登録機能の作成

--- app/sns_project/accounts/forms.py
+++ app/sns_project/accounts/forms.py
@@ -1,6 +1,10 @@
+from django import forms
 from django.contrib.auth.forms import (
-    AuthenticationForm
+    AuthenticationForm, UserCreationForm
 )
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 
 class LoginForm(AuthenticationForm):
@@ -11,3 +15,20 @@ class LoginForm(AuthenticationForm):
         for field in self.fields.values():
             field.widget.attrs['class'] = 'form-control'
             field.widget.attrs['placeholder'] = field.label  # placeholderにフィールドのラベルを入れる
+
+class UserCreateForm(UserCreationForm):
+    """ユーザー登録用フォーム"""
+
+    class Meta:
+        model = User
+        fields = ('email',)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs['class'] = 'form-control'
+
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        User.objects.filter(email=email, is_active=False).delete()
+        return email
--- app/sns_project/accounts/urls.py
+++ app/sns_project/accounts/urls.py
@@ -2,10 +2,15 @@ from django.urls import path
 
 from . import views
 
+app_name = 'accounts'
+
 urlpatterns = [
     path('test/', views.test, name='test'),
     path('signup/', views.signup, name='signup'),
-    path('login/', views.Login.as_view(), name='login'),
-    path('logout/', views.Logout.as_view(), name='logout'),
-    path('profile/', views.Profile.as_view(), name='profile'),
+    path('login/', views.Login, name='login'),
+    path('logout/', views.Logout, name='logout'),
+    path('profile/', views.Profile, name='profile'),
+    path('user_create/', views.UserCreate, name='user_create'),
+    path('user_create/done', views.UserCreateDone, name='user_create_done'),
+    path('user_create/complete/<token>/', views.UserCreateComplete, name='user_create_complete'),
 ]
