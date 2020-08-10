0f9fa22cdbb15bd28e6392f7be0eff261aa02b49
niya1123 <yoheikurioka@gmail.com>
2020-08-10 16:32:14 +0900

    とりあえずコンテンツモデルのプロトタイプと自動adminの追加の実装

--- app/sns_project/contents/admin.py
+++ app/sns_project/contents/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
-
+from .models import Tag, Contents, Review
 # Register your models here.
+
+class ContentsAdmin(admin.ModelAdmin):
+    search_fields = ('title', 'text')
+    list_display = ['title', 'is_public', 'updated_at', 'created_at', 'title_len']
+    list_filter = ['is_public', 'tags']
+    ordering = ('-updated_at',)
+
+    def title_len(self, obj):
+        return len(obj.title)
+
+    title_len.short_description = 'タイトルの文字数'
+
+admin.site.register(Contents, ContentsAdmin)
+admin.site.register(Tag)
+admin.site.register(Review)
--- app/sns_project/contents/models.py
+++ app/sns_project/contents/models.py
@@ -1,27 +1,42 @@
-# from django.db import models
-# from django.utils import timezone
+from django.db import models
+from django.utils import timezone
 
-# class Tag(models.Model):
-#     """
-#     タグモデル
-#     """
-#     name = models.CharField('タグ名', max_length=255, unique=True)
+class Tag(models.Model):
+    """
+    タグモデル
+    """
+    name = models.CharField('タグ名', max_length=255, unique=True)
 
-#     def __str__(self):
-#         return self.name
+    def __str__(self):
+        return self.name
 
-# class Contents(models.Model):
-#     """コンテンツモデル"""
-#     title = models.CharField('タイトル', max_length=32)
-#     text = models.TextField('本文')
-#     tags = models.ManyToManyField(Tag, verbose_name='タグ', blank=True)
+class Contents(models.Model):
+    """コンテンツモデル"""
+    title = models.CharField('タイトル', max_length=50)
+    tags = models.ManyToManyField(Tag, verbose_name='タグ', blank=True)
+    question_text = models.TextField('本文')
 
-#     relation_posts = models.ManyToManyField('self', verbose_name='関連記事', blank=True)
-#     is_public = models.BooleanField('公開可能か?', default=True)
-#     description = models.TextField('記事の説明', max_length=130)
-#     keywords = models.CharField('記事のキーワード', max_length=255, default='記事のキーワード')
-#     created_at = models.DateTimeField('作成日', default=timezone.now)
-#     updated_at = models.DateTimeField('更新日', default=timezone.now)
+    relation_posts = models.ManyToManyField('self', verbose_name='関連記事', blank=True)
+    is_public = models.BooleanField('公開可能か', default=True)
+    question_description = models.TextField('問題の説明', max_length=130)
+    keywords = models.CharField('問題のキーワード', max_length=255, default='問題のキーワード')
+    created_at = models.DateTimeField('作成日', default=timezone.now)
+    updated_at = models.DateTimeField('更新日', default=timezone.now)
 
-#     def __str__(self):
-#         return self.title
+    def __str__(self):
+        return self.title
+
+class Review(models.Model):
+    """評価"""
+    SCORE_CHOICES = (
+        (1, '★1'),
+        (2, '★2'),
+        (3, '★3'),
+        (4, '★4'),
+        (5, '★5'),
+    )
+    point = models.IntegerField('評価点', choices=SCORE_CHOICES)
+    target = models.ForeignKey(Contents, verbose_name='評価対象の本', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return '{} - {}'.format(self.target.title, self.get_point_display())
