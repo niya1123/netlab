bb796d16dfda352c8429b6d502147750b69b5655
niya1123 <yoheikurioka@gmail.com>
2020-08-17 15:18:46 +0900

    authorとか追加

--- app/sns_project/contents/models.py
+++ app/sns_project/contents/models.py
@@ -16,6 +16,7 @@ class Content(models.Model):
     """コンテンツモデル"""
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     title = models.CharField('タイトル', max_length=50)
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
     tags = models.ManyToManyField(Tag, verbose_name='タグ', blank=True)
     question_text = models.TextField('問題文')
 
--- app/sns_project/contents/views.py
+++ app/sns_project/contents/views.py
@@ -1,13 +1,18 @@
 from django.urls import reverse_lazy
 from django.views import generic
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .forms import CreateContentForm
 
-class CreateContent(generic.CreateView):
+class CreateContent(LoginRequiredMixin, generic.edit.CreateView):
     """コンテンツ作成ビュー"""
     form_class = CreateContentForm
     template_name = 'contents/create_content.html'
     success_url = reverse_lazy('contents:create_content_done')
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
 
 class CreateContentDone(generic.TemplateView):
     """コンテンツ投稿完了を知らせる"""
