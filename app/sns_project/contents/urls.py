47590f28c9c2e641b31d7cdac256fa7b103f9e7d
niya1123 <yoheikurioka@gmail.com>
2020-09-14 15:43:20 +0900

    urlにquestion_listの追加

--- app/sns_project/contents/urls.py
+++ app/sns_project/contents/urls.py
@@ -5,6 +5,7 @@ from . import views
 app_name = 'contents'
 
 urlpatterns = [
+    path('content/detail/<uuid:pk>/question/list', views.QuestionList.as_view(), name="question_list"),
     path('content/detail/<uuid:pk>/question/new', views.CreateQuestion.as_view(), name="add_question"),
     path('content/create/tag', views.AddTag.as_view(), name='add_tag'),
     path('content/detail/<uuid:pk>', views.ContentDetail.as_view(), name='content_detail'), 
